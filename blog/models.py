761129635cde807276e8fdb205af6dfa82e7d6a2
baljeet <yadavbaljeet91@gmail.com>
2017-01-10 14:10:57 +0530

    migration + template changes

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
 	author = models.ForeignKey('auth.User')
 	title = models.CharField(max_length = 200)
 	text = models.TextField()
-	created_date = models.DateTimeField(blank = True, null = True)
+	published_date = models.DateTimeField(blank = True, null = True)
 
 	def publish(self):
 		self.published_date = timezone.now()
@@ -13,4 +13,4 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
-	
+
